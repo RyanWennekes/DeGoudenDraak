7cae4db4a219a20aaf9e54410a4e199c2f2123ad
Ryan Wennekes <ryanwennekes@gmail.com>
2020-05-14 12:16:32 +0200

    Added migrations

---
+++ storage/framework/views/3fed27e39455d6eb68c8e8bf8589ba79944ea30b.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts TODO: remove fonts.googleapis and use font-awesome -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet" type="text/css">
+
+    <!-- Javascript -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+</head>
+<body>
+<div id="app">
+    <vue-app>
+        
+    </vue-app>
+</div>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\College\Year 2\WEBFS\DeGoudenDraak_Development\resources\views/index.blade.php ENDPATH**/ ?>
