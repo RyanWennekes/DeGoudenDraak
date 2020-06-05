ad72606785a1bfabdbcca787f86a3320187b7c79
Martijn Zentjens <mbp.zentjens@student.avans.nl>
2020-06-05 12:48:07 +0200

    Created OrderTable example

---
+++ resources/js/components/admin/order/OrderTable.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- TODO : Loading and placing products here -->
+    <v-list two-line subheader>
+        <v-subheader>Producten</v-subheader>
+
+        <template v-for="index in 20">
+            <v-list-item :key="`product-${index}`">
+                <v-list-item-content>
+                    <v-list-item-title>title</v-list-item-title>
+                    <v-list-item-subtitle>subtitle</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-action>
+                    <v-btn fab dark small color="blue">
+                        <v-icon small>fa-plus</v-icon>
+                    </v-btn>
+                </v-list-item-action>
+            </v-list-item>
+
+            <!-- This divide items from each other-->
+            <v-divider :key="`divider-${index}`"></v-divider>
+        </template>
+    </v-list>
+</template>
+
+<script>
+export default {
+    name: 'OrderTable',
+};
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/pages/admin/OrderResource.vue
+++ resources/js/pages/admin/OrderResource.vue
@@ -1,13 +1,31 @@
 <template>
-    <p>resource</p>
+    <v-row align="stretch">
+        <!-- Overview products -->
+        <v-col cols="12" md="7" lg="8" class="full-height overflow">
+            <OrderTable/>
+        </v-col>
+
+        <!-- Creating order -->
+        <v-col cols="12" md="5" lg="4" class="full-height">
+            <OrderForm/>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
-    export default {
-        name: 'OrderResource',
-    };
+import OrderForm from '../../components/admin/order/OrderForm.vue';
+import OrderTable from '../../components/admin/order/OrderTable.vue';
+
+export default {
+    name: 'OrderResource',
+    components: {OrderTable, OrderForm},
+};
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.overflow {
+    overflow-y: scroll;
+    position: absolute;
+    max-height: 97%;
+}
 </style>
