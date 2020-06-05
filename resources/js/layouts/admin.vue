58432376a6503aecee0cdb64ce797b93113bb8e9
Martijn Zentjens <mbp.zentjens@student.avans.nl>
2020-06-05 12:02:16 +0200

    Create basic admin-layout

--- resources/js/layouts/admin.vue
+++ resources/js/layouts/admin.vue
@@ -1,5 +1,13 @@
 <template>
-    <router-view></router-view>
+    <v-row justify="center" align="stretch" class="primary pa-6">
+        <v-col cols="12" md="10" class="full-height">
+            <v-card height="100%">
+                <v-card-text>
+                    <router-view></router-view>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
