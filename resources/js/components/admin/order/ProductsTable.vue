6c942aba4ef82df0a1df870ff619dc9e484793cc
Martijn Zentjens <mbp.zentjens@student.avans.nl>
2020-06-06 12:48:03 +0200

    Changed table

--- resources/js/components/admin/order/ProductsTable.vue
+++ resources/js/components/admin/order/ProductsTable.vue
@@ -1,30 +1,36 @@
 <template>
     <!-- TODO : Loading and placing products here -->
-    <v-list two-line subheader>
-        <v-subheader>Producten</v-subheader>
+    <div>
+        <v-list two-line subheader v-for="category in categories" :key="category.id">
+            <v-subheader class="justify-center">{{category.name}}</v-subheader>
 
-        <template v-for="(product, index) in products">
-            <v-list-item :key="`product-${index}`" class="product">
-                <v-list-item-content>
-                    <v-list-item-title>{{product.name}}</v-list-item-title>
-                    <v-list-item-subtitle>{{product.description}}</v-list-item-subtitle>
-                </v-list-item-content>
+            <template v-for="(product, index) in products" v-if="product.categoryId === category.id">
+                <v-list-item :key="`product-${index}`" class="product">
+                    <v-list-item-content class="id-label">
+                        {{product.id}}.
+                    </v-list-item-content>
 
-                <v-list-item-content class="price-label">
-                    {{product.price | currency}}
-                </v-list-item-content>
+                    <v-list-item-content>
+                        <v-list-item-title>{{product.name}}</v-list-item-title>
+                        <v-list-item-action-text class="pr-2">{{product.description}}</v-list-item-action-text>
+                    </v-list-item-content>
 
-                <v-list-item-action>
-                    <v-btn fab dark small color="blue">
-                        <v-icon small>fa-plus</v-icon>
-                    </v-btn>
-                </v-list-item-action>
-            </v-list-item>
+                    <v-list-item-content class="price-label">
+                        {{product.price | currency}}
+                    </v-list-item-content>
 
-            <!-- This divide items from each other-->
-            <v-divider :key="`divider-${index}`"></v-divider>
-        </template>
-    </v-list>
+                    <v-list-item-action>
+                        <v-btn fab dark small color="blue">
+                            <v-icon small>fa-plus</v-icon>
+                        </v-btn>
+                    </v-list-item-action>
+                </v-list-item>
+
+                <!-- This divide items from each other-->
+                <v-divider :key="`divider-${index}`"></v-divider>
+            </template>
+        </v-list>
+    </div>
 </template>
 
 <script>
@@ -68,79 +74,79 @@ export default {
                     id: 1,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 1,
+                    categoryId: 1,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 1,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 1,
+                    categoryId: 1,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 1,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 1,
+                    categoryId: 1,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 1,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 1,
+                    categoryId: 1,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 1,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 1,
+                    categoryId: 1,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 2,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 2,
+                    categoryId: 2,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 2,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 2,
+                    categoryId: 2,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 2,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 2,
+                    categoryId: 2,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 2,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 2,
+                    categoryId: 2,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 2,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 2,
+                    categoryId: 2,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 2,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 2,
+                    categoryId: 2,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 2,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 2,
+                    categoryId: 2,
                 }, {
                     name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     id: 2,
                     price: 5.00,
                     description: 'Aliquam a rutrum tortor. Nullam porta orci molestie, volutpat sapien nec, malesuada nibh. Cras ipsum tellus, fermentum at luctus vitae, mattis eget ante. Donec sit amet nulla a nisi molestie vulputate. Donec ornare, nisl at bibendum venenatis, turpis ipsum interdum metus, finibus dapibus dui ligula et orci.',
-                    category: 2,
+                    categoryId: 2,
                 },
             ];
         },
@@ -150,6 +156,10 @@ export default {
 
 <style lang="scss">
 .product {
+    .id-label {
+        max-width: 30px;
+        text-align: center;
+    }
     .price-label {
         max-width: 50px;
         text-align: center;
