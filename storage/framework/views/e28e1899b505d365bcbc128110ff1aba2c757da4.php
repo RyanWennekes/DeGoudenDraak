d064dc7b24e517d12dc82bf6c89cff1d4ff68fa2
Martijn Zentjens <mbp.zentjens@student.avans.nl>
2020-06-06 11:55:02 +0200

    Removed unnecessary code

--- storage/framework/views/e28e1899b505d365bcbc128110ff1aba2c757da4.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php $__env->startSection('content'); ?>
-    <vue-app></vue-app>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Martijn\Documents\Avans hogeschool\Blok8\WEBFS\DeGoudenDraak\resources\views/index.blade.php ENDPATH**/ ?>
