e935af7b1d65c40c9646ac9172b98be7bd32d9e3
Ryan Wennekes <ryanwennekes@gmail.com>
2020-06-07 12:51:25 +0200

    Naming convention, disabled old user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserSeeder::class);
+         $this->call(UsersTableSeeder::class);
          $this->call(TableSeeder::class);
          $this->call(OrderSeeder::class);
          $this->call(ProductTypeSeeder::class);
