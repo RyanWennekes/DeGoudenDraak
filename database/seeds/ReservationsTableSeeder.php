bed83b3434327cd98ddf85a7ab00591f3ce615ed
Martijn Zentjens <mbp.zentjens@student.avans.nl>
2020-06-07 19:02:09 +0200

    Using realistisch name

--- database/seeds/ReservationsTableSeeder.php
+++ database/seeds/ReservationsTableSeeder.php
@@ -16,10 +16,12 @@ class ReservationsTableSeeder extends Seeder
         foreach (Table::allTables() as $key => $table) {
             for ($x = 0; $x < rand(0, 3); $x++) {
                 $reservationDate = Carbon\Carbon::now()->addDays($x);
+                $faker = Faker\Factory::create();
 
                 DB::table('reservations')->insert([
-                    'check_in' => $reservationDate->format('Y-m-d H:m:s'),
-                    'table_id' => $table->id,
+                    'check_in'      => $reservationDate->format('Y-m-d H:m:s'),
+                    'table_id'      => $table->id,
+                    'customer_name' => $faker->unique()->name,
                 ]);
             }
         }
