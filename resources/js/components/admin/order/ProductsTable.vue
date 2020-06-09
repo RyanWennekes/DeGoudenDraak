fabf205ca40c12398654c20122b457df6c31fd0a
Martijn Zentjens <mbp.zentjens@student.avans.nl>
2020-06-09 16:57:42 +0200

    Filtering with category

--- resources/js/components/admin/order/ProductsTable.vue
+++ resources/js/components/admin/order/ProductsTable.vue
@@ -3,21 +3,43 @@
         <h2 class="text-center">Producten</h2>
         <v-divider class="my-3"/>
 
-        <v-text-field
-            v-model="search"
-            label="Zoeken"
-            class="searchbar"
-            hide-details
-            filled>
-            <template #prepend-inner>
-                <v-icon color="primary" class="mr-2">fa-search</v-icon>
-            </template>
-        </v-text-field>
+        <!-- Searching / Filtering -->
+        <v-row class="ma-0">
+            <v-col cols="12" sm="8" class="pa-0" :class="{'pr-1' : $vuetify.breakpoint.smAndUp}">
+                <!-- Searching on name or product-number -->
+                <v-text-field
+                    v-model="search"
+                    label="Zoeken"
+                    class="searchbar"
+                    hide-details
+                    filled>
+                    <template #prepend-inner>
+                        <v-icon color="primary" class="mr-2">fa-search</v-icon>
+                    </template>
+                </v-text-field>
+            </v-col>
+            <v-col cols="12" sm="4" class="pa-0" :class="{'pl-1' : $vuetify.breakpoint.smAndUp}">
+                <!-- Filtering on producttype -->
+                <v-select
+                    v-model="selectedCategory"
+                    :items="categories"
+                    label="Categorie"
+                    item-text="type_nl"
+                    item-value="id"
+                    hide-details
+                    filled>
+                    <template #prepend-inner>
+                        <v-icon color="primary" class="mr-2">fa-filter</v-icon>
+                    </template>
+                </v-select>
+            </v-col>
+        </v-row>
 
         <div class="overflow" :class="{'small' : $vuetify.breakpoint.smAndDown}">
             <v-list two-line subheader v-for="category in categories" :key="category.id" v-show="!loading">
-                <v-subheader class="justify-center">{{category.type_nl}} <span class="font-weight-bold ml-1"
-                                                                               v-if="category.has_rice">(met rijst)</span>
+                <v-subheader class="justify-center">{{category.type_nl}}
+                    <span class="font-weight-bold ml-1"
+                          v-if="category.has_rice">(met rijst)</span>
                 </v-subheader>
 
                 <template v-for="(product, index) in filteredProducts" v-if="product.product_type_id === category.id">
@@ -69,6 +91,7 @@ export default {
             categories: null,
             loading: false,
             search: '',
+            selectedCategory: '',
         };
     },
     created() {
@@ -77,7 +100,12 @@ export default {
     },
     computed: {
         filteredProducts() {
-            return this.products != null ? this.products.filter((product) => product.name.toLowerCase().includes(this.search.toLowerCase()) || String(product.id).includes(this.search)) : null;
+            return this.products != null ? this.products.filter((product) => {
+                if (!this.selectedCategory || this.selectedCategory && this.selectedCategory === product.product_type_id)
+                    return product.name.toLowerCase().includes(this.search.toLowerCase()) || String(product.id).includes(this.search);
+
+                return false;
+            }) : null;
         },
     },
     methods: {
