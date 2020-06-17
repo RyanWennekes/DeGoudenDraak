d6a7cfce637b2ea6bee1699264d2f18ed557fe56
Ryan Wennekes <ryanwennekes@gmail.com>
2020-06-17 16:26:27 +0200

    styling

--- resources/js/pages/guest/Table.vue
+++ resources/js/pages/guest/Table.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
-        <ul>
-            <li v-for="category in categories" @click.prevent="toggle(category)">
+        <h1>Bestellen</h1>
+        <ul class="categories">
+            <li v-for="category in categories" @click.self="toggle(category)">
                 {{ category.type_nl }}
-                <table v-show="category.show">
+                <table v-show="category.show" class="products">
                     <tr>
                         <th>Naam</th>
                         <th>Pittigheid</th>
@@ -66,8 +67,27 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    h1 {
+        text-align: center;
+    }
+    ul.categories {
+        list-style-type: none;
+        padding: 0;
+        text-align: center;
+    }
     li:hover {
         cursor: pointer;
+    }
+
+    table.products {
+        border-spacing: 20px;
+        width: 1000px;
+        margin: 0 auto;
+        text-align: right;
+
+        td:first-child, th:first-child {
+            text-align: left;
+        }
     }
 </style>
